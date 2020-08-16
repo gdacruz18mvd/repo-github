f44199c055646331d45577facabecc2b98e79a1b
gdacruz18mvd <68927364+gdacruz18mvd@users.noreply.github.com>
2020-08-16 17:26:37 -0300

    Entrega 1
    Commit entrega 1 final

--- js/login.js
+++ js/login.js
@@ -1,6 +1,35 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+function login() {
+    var user = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+
+    var useralert = document.getElementById("alertauser");
+    var passwordalert = document.getElementById("alertapassword");
+
+    if (user.trim() !== "") {
+        useralert.style.visibility="hidden";
+        useralert.style.color= "yellow";
+    }
+    if (user.trim() === "") {
+        useralert.style.visibility="visible";
+        useralert.style.color= "yellow";
+    }
+    if (password.trim() !== "") {
+        passwordalert.style.visibility="hidden";
+        passwordalert.style.color= "yellow";
+    }
+    if (password.trim() === "") {
+        passwordalert.style.visibility="visible";
+        passwordalert.style.color= "yellow";
+    }
+    if (user.trim() !== "" && password.trim() !== "") {
+        window.location="cover.html";
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function(e){
+    
 });
